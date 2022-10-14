2af3c21c9d3e355467e34842b728f0c176fec459
EvelynTee <temitayoyesufu@gmail.com>
2022-10-13 21:00:52 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Return: Always 0
  */
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
+{
+	printf("with proper grammar, but thr outcome is a piece of art,\n");
 	return (0);
 }
