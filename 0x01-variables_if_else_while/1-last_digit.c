e1ec77afa71271828fd73708e47a417f1432a2ec
EvelynTee <temitayoyesufu@gmail.com>
2022-10-21 20:44:12 -0700

    Print_last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 /**
- * main - Entry
+ * main - > assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
  * Return: Always 0
  */
 int main(void)
