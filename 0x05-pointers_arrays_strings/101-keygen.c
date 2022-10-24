94bcd801806bad8d31fa5639cca4fe6a63bff4d8
EvelynTee <temitayoyesufu@gmail.com>
2022-10-24 13:41:46 -0700

    Task11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -32,7 +32,15 @@ int main(void)
 
 		for (index = 0; password[index]; index++)
 		{
-			if (paasword[index] >= (33 + diff_half2))
+			if (paasword[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
 			{
 				password[index] -= diff_half2;
 				break;
