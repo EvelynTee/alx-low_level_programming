3de076de96788de7795d8cface4e625f09a7ce0c
EvelynTee <temitayoyesufu@gmail.com>
2022-11-12 08:24:13 -0800

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion - prints a string in reverse.
+ * @s: string
+ * Return: no return
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
